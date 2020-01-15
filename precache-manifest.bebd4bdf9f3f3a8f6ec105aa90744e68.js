b0e2526d3683936b6c59a499cd3f22ec47ebae6a
Mojeedkush <mkusimo90@gmail.com>
2020-01-15 16:40:57 +0100

    Updates

---
+++ precache-manifest.bebd4bdf9f3f3a8f6ec105aa90744e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d8fbdbd33ba4c3308e36b80808660f",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "1e234d60d89ae9aadb4c",
+    "url": "/random-quote-machine/static/css/main.28bd294a.chunk.css"
+  },
+  {
+    "revision": "b3de2737b3be5f52c1f6",
+    "url": "/random-quote-machine/static/js/2.8c6924bf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/random-quote-machine/static/js/2.8c6924bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e234d60d89ae9aadb4c",
+    "url": "/random-quote-machine/static/js/main.375dbe95.chunk.js"
+  },
+  {
+    "revision": "afc9b93c50eb3e83906a",
+    "url": "/random-quote-machine/static/js/runtime-main.daced29d.js"
+  }
+]);
