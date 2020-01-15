66a3af3a575a219701f09045d2f81b04ce2aeceb
Mojeedkush <mkusimo90@gmail.com>
2020-01-15 16:37:35 +0100

    Initial set up of Random qoute machine

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './bootstrap.css';
 import * as serviceWorker from './serviceWorker';
+
+let bodyBgr = {backgroundColor: 'red'};
+let bodyBgb = {backgroundColor: 'blue'};
+const bodyBgg = {backgroundColor: 'green'};
+class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            click: true
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleChange () {
+
+    }
+
+    handleClick () {
+        if (this.state.click) {
+            bodyBgr = bodyBgb;
+            this.setState({
+                click : false
+            });
+        } else {
+            bodyBgr = bodyBgr;
+            this.setState({
+                click : true
+            });
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1 className="text-center">
+                    A Random Qoute Machine
+                </h1>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div  className="col-3"></div>
+                        <div  style={bodyBgr} className="col-6 btn-div">
+                            <button className="btn bg-light" onClick={this.handleClick}>New qoute</button>
+                        </div>
+                        <div className="col-3"></div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
