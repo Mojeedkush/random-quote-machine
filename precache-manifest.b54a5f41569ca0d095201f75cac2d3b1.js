ef7974df031171f74a8a5676fdbe2dd9ec9b81d4
Mojeedkush <mkusimo90@gmail.com>
2020-01-15 14:17:52 +0100

    Updates

---
+++ precache-manifest.b54a5f41569ca0d095201f75cac2d3b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2a7f56d555058071a5cfcab33c56ee",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "3e464a9af1efecf7e7ef",
+    "url": "/random-quote-machine/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b057a7017ad522a1b0d6",
+    "url": "/random-quote-machine/static/js/2.548c239a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/random-quote-machine/static/js/2.548c239a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e464a9af1efecf7e7ef",
+    "url": "/random-quote-machine/static/js/main.b05eac4f.chunk.js"
+  },
+  {
+    "revision": "afc9b93c50eb3e83906a",
+    "url": "/random-quote-machine/static/js/runtime-main.daced29d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quote-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
